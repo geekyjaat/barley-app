4919857b382cbaf37482bb6a34706720fa07223a
vikram gulia <vgulia@hawk.iit.edu>
2017-12-05 14:04:06 -0700

    commented test case out

--- src/test/java/com/barley/dao/DaoAccessTest.java
+++ src/test/java/com/barley/dao/DaoAccessTest.java
@@ -2,7 +2,7 @@ package com.barley.dao;
 
 public class DaoAccessTest {
     public static void main(String[] args) throws Exception {
-    	MongoDb mongoDb = new MongoDb("barley");
-        System.out.println(mongoDb.getLinks());
+    	//MongoDb mongoDb = new MongoDb("barley");
+        //System.out.println(mongoDb.getLinks());
     }
 }
